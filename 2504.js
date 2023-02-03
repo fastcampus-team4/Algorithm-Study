76dbdb54dd40e95fc8f7ed38895c19c7a70b1357
Wonny-ing <h.wonny11@gmail.com>
2023-02-03 16:08:15 +0900

    fix: 2504.js

--- 2504.js
+++ 2504.js
@@ -1,58 +1,55 @@
-const filePath = process.platform === 'linux' ? 0 : './input.txt';
-let array = require('fs').readFileSync(filePath).toString().trim().split('');
+// https://www.acmicpc.net/problem/2504
+const filePath = process.platform === 'linux' ? 0 : 'input.txt';
+const input = require('fs').readFileSync(filePath).toString().split('');
+input;
+function solution(input) {
+  let stack = [];
+  let answer = 0;
 
-let stack = [];
+  for (let i = 0; i < input.length; i++) {
+    if (input[i] === '(' || input[i] === '[') {
+      stack.push(input[i]);
+    }
+    if (input[i] === ')') {
+      if (stack[stack.length - 1] === '(') {
+        stack.pop();
+        stack.push(2);
+      } else if (stack[stack.length - 1] !== '[' && stack[stack.length - 2] === '(') {
+        const x = stack.pop();
+        stack.pop();
+        stack.push(x * 2);
+      } else {
+        return answer;
+      }
+    }
 
-for (let i = 0; i < array.length; i++) {
-  if (array[i] === '(' || array[i] === '[') {
-    stack.push(array[i]);
-  }
-  if (array[i] === ')') {
-    if (stack[stack.length - 1] === '(') {
+    if (input[i] === ']') {
+      if (input[i - 1] === '[') {
+        stack.pop();
+        stack.push(3);
+      } else if (stack[stack.length - 1] !== '(' && stack[stack.length - 2] === '[') {
+        const popNumber = stack.pop();
+        stack.pop();
+        stack.push(popNumber * 3);
+      } else {
+        return answer;
+      }
+    }
+
+    if (typeof stack[stack.length - 1] === 'number' && typeof stack[stack.length - 2] === 'number') {
+      const num = stack[stack.length - 1] + stack[stack.length - 2];
       stack.pop();
-      stack.push(2);
-    } else if (
-      stack[stack.length - 1] !== '[' &&
-      stack[stack.length - 2] === '('
-    ) {
-      const x = stack.pop();
       stack.pop();
-      stack.push(x * 2);
-    } else {
-      console.log(0);
-      return;
+      stack.push(num);
     }
   }
-  if (array[i] === ']') {
-    if (stack[stack.length - 1] === '[') {
-      stack.pop();
-      stack.push(3);
-    } else if (
-      stack[stack.length - 1] !== '(' &&
-      stack[stack.length - 2] === '['
-    ) {
-      const x = stack.pop();
-      stack.pop();
-      stack.push(x * 3);
-    } else {
-      console.log(0);
-      return;
-    }
+
+  if (stack.length !== 1 || typeof stack[0] !== 'number') {
+    answer = 0;
+  } else {
+    answer = stack[0];
   }
 
-  if (
-    typeof stack[stack.length - 1] === 'number' &&
-    typeof stack[stack.length - 2] === 'number'
-  ) {
-    const num = stack[stack.length - 1] + stack[stack.length - 2];
-    stack.pop();
-    stack.pop();
-    stack.push(num);
-  }
+  return answer;
 }
-
-if (stack.length !== 1 || typeof stack[0] !== 'number') {
-  console.log(0);
-} else {
-  console.log(stack[0]);
-}
+console.log(solution(input));
